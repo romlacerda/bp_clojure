5e08dc83d5b7916b317ce5557ce8db6114d6e824
Romulo Lacerda <rlcrweb@gmail.com>
2020-09-27 21:22:33 -0300

    feat: mecenas-test

--- src/takehome/core.clj
+++ src/takehome/core.clj
@@ -3,21 +3,31 @@
 
 
 (defn patriota-access? [object purchase]
-  (or (= (:type object) :podcast) (= (:type object) :debate) (and (= (:type object) :interview) 
-                                                                    (time/before? (:subscription-start purchase)
-                                                                    (:released-at object)
-                                                                    (:subscription-end purchase)))))
-
-(defn premium-access? [object purchase]
-  (or (= (:type object) :course) (= (:type object) :podcast) (= (:type object) :debate) (and (= (:type object) :interview)
+  (or (= (:type object) :podcast) (= (:type object) :debate) (and (= (:type object) :interview)
                                                                   (time/before? (:subscription-start purchase)
                                                                                 (:released-at object)
                                                                                 (:subscription-end purchase)))))
-                   
+
+(defn premium-access? [object purchase]
+  (or (= (:type object) :course) (= (:type object) :podcast) (= (:type object) :debate) (and (= (:type object) :interview)
+                                                                                             (time/before? (:subscription-start purchase)
+                                                                                                           (:released-at object)
+                                                                                                           (:subscription-end purchase)))))
+(defn mecenas-access? [object purchase]
+  (or (= (:type object) :course) (= (:type object) :podcast) (= (:type object) :debate) (and (= (:type object) :interview)
+                                                                                             (time/before? (:subscription-start purchase)
+                                                                                                           (:released-at object)
+                                                                                                           (:subscription-end purchase)))
+      (and (= (:type object) :patron)
+           (time/before? (:subscription-start purchase)
+                         (:released-at object)
+                         (:subscription-end purchase)))))
+
 (defn can-access? [object purchase]
-  (cond 
-        (= (:type purchase) :patriota) (patriota-access? object purchase)
-        (= (:type purchase) :premium) (premium-access? object purchase)))
+  (cond
+    (= (:type purchase) :patriota) (patriota-access? object purchase)
+    (= (:type purchase) :premium) (premium-access? object purchase)
+    (= (:type purchase) :mecenas) (mecenas-access? object purchase)))
         ;; (:type :premium (premium-access? object purchase))))
   ;; (if (= (:type purchase) :patriota) 
   ;;   (patriota-access? object purchase)))
