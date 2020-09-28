7c770a887e1e90f036b6e2323fa310e20a015e64
Romulo Lacerda <rlcrweb@gmail.com>
2020-09-28 11:29:40 -0300

    feat: finishing tests

--- src/takehome/core.clj
+++ src/takehome/core.clj
@@ -3,39 +3,44 @@
 
 
 (defn patriota-access? [object purchase]
-  (or (= (:type object) :podcast) (= (:type object) :debate) (and (= (:type object) :interview)
-                                                                  (time/before? (:subscription-start purchase)
-                                                                                (:released-at object)
-                                                                                (:subscription-end purchase)))))
+  (or 
+   (= (:type object) :series)
+   (= (:type object) :podcast)
+   (= (:type object) :debate)
+   (and (= (:type object) :interview)
+        (time/before? (:subscription-start purchase)
+                      (:released-at object)
+                      (:subscription-end purchase)))))
 
 (defn premium-access? [object purchase]
-  (or (= (:type object) :course) (= (:type object) :podcast) (= (:type object) :debate) (and (= (:type object) :interview)
-                                                                                             (time/before? (:subscription-start purchase)
-                                                                                                           (:released-at object)
-                                                                                                           (:subscription-end purchase)))))
+  (or
+   (= (:type object) :series) 
+   (= (:type object) :course) 
+   (= (:type object) :podcast) 
+   (= (:type object) :debate) 
+   (and (= (:type object) :interview)
+        (time/before? (:subscription-start purchase)
+                      (:released-at object)
+                      (:subscription-end purchase)))))
+
 (defn mecenas-access? [object purchase]
-  (or (= (:type object) :course) (= (:type object) :podcast) (= (:type object) :debate) (and (= (:type object) :interview)
-                                                                                             (time/before? (:subscription-start purchase)
-                                                                                                           (:released-at object)
-                                                                                                           (:subscription-end purchase)))
-      (and (= (:type object) :patron)
-           (time/before? (:subscription-start purchase)
-                         (:released-at object)
-                         (:subscription-end purchase)))))
+  (or
+   (= (:type object) :series)
+   (= (:type object) :course) 
+   (= (:type object) :podcast) 
+   (= (:type object) :debate) 
+   (and (= (:type object) :interview)
+        (time/before? (:subscription-start purchase)
+        (:released-at object)
+        (:subscription-end purchase)))
+   (and (= (:type object) :patron)
+        (time/before? (:subscription-start purchase)
+                      (:released-at object)
+                      (:subscription-end purchase)))))
+
 
 (defn can-access? [object purchase]
   (cond
     (= (:type purchase) :patriota) (patriota-access? object purchase)
     (= (:type purchase) :premium) (premium-access? object purchase)
     (= (:type purchase) :mecenas) (mecenas-access? object purchase)))
-        ;; (:type :premium (premium-access? object purchase))))
-  ;; (if (= (:type purchase) :patriota) 
-  ;;   (patriota-access? object purchase)))
-
-  ; (if (= (:type purchase) :patriota)
-  ;   (and 
-  ;     (or (= (:type object) :podcast) (= (:type object) :debate) (and (= (:type object) :interview) 
-  ;                                                                     (time/before? (:subscription-start purchase)
-  ;                                                                                   (:released-at object)
-  ;                                                                                   (:subscription-end purchase))))))                                                   
-;; )
--- test/takehome/core_test.clj
+++ test/takehome/core_test.clj
@@ -3,6 +3,7 @@
             [java-time :as time]
             [takehome.core :as sub]))
 
+
 (deftest test-patriota
   (are [result object] (= result
                             (sub/can-access?
@@ -15,8 +16,8 @@
     true { :type :interview :name "Congresso Brasil Paralelo - Rafael Nogueira", :released-at (time/local-date-time "2019-11-16T21:40:51.579") }
     false { :type :movie :name "Há regras para educar os filhos?", :released-at (time/local-date-time "2020-03-29T20:02:34.356") }
     false { :type :interview :name "Congresso Brasil Paralelo - Rafael Nogueira", :released-at (time/local-date-time "2020-11-16T21:40:51.579") }
-    false {:type :course :name "Carlos Lacerda e a República Brasileira", :released-at "2018-11-21T18:32:53.278"} ))
-
+    false { :type :course :name "Carlos Lacerda e a República Brasileira", :released-at "2018-11-21T18:32:53.278" } 
+    false { :type :patron :name "Relatório Mecenas", :released-at (time/local-date-time "2020-08-10T20:00:00.656") } ))
 
 (deftest test-premium
   (are [result object] (= result
@@ -30,7 +31,8 @@
     true { :type :interview :name "Congresso Brasil Paralelo - Rafael Nogueira", :released-at (time/local-date-time "2018-11-16T21:40:51.579") }
     true { :type :course :name "Carlos Lacerda e a República Brasileira", :released-at "2018-11-21T18:32:53.278" }
     false { :type :movie :name "Há regras para educar os filhos?", :released-at (time/local-date-time "2020-03-29T20:02:34.356") }
-    false { :type :interview :name "Congresso Brasil Paralelo - Rafael Nogueira", :released-at (time/local-date-time "2020-11-16T21:40:51.579") }))
+    false { :type :interview :name "Congresso Brasil Paralelo - Rafael Nogueira", :released-at (time/local-date-time "2020-11-16T21:40:51.579") }
+    false { :type :patron :name "Relatório Mecenas", :released-at (time/local-date-time "2020-08-10T20:00:00.656") } ))
 
 (deftest test-mecenas
   (are [result object] (= result
@@ -43,6 +45,10 @@
     true { :type :debate :name "Há regras para educar os filhos?", :released-at (time/local-date-time "2018-03-29T20:02:34.356") }
     true { :type :interview :name "Congresso Brasil Paralelo - Rafael Nogueira", :released-at (time/local-date-time "2019-11-16T21:40:51.579") }
     true { :type :course :name "Carlos Lacerda e a República Brasileira", :released-at (time/local-date-time "2018-11-21T18:32:53.278") }
-    true { :type :patron :name "Relatório Mecenas", :released-at (time/local-date-time "2020-08-10T20:00:00.656") }))
-    ;; false { :type :movie :name "Há regras para educar os filhos?", :released-at (time/local-date-time "2020-03-29T20:02:34.356") }
+    true { :type :patron :name "Relatório Mecenas", :released-at (time/local-date-time "2020-08-10T20:00:00.656") } 
+    false { :type :movie :name "Há regras para educar os filhos?", :released-at (time/local-date-time "2020-03-29T20:02:34.356") }
+    false { :type :interview :name "Congresso Brasil Paralelo - Alexandre Borges", :released-at (time/local-date-time "2020-11-16T21:40:51.621" ) }
+    false { :type :patron :name "Relatório Mecenas", :released-at (time/local-date-time "2018-08-10T20:00:00.656") } ))
+
+
     ;; false { :type :interview :name "Congresso Brasil Paralelo - Rafael Nogueira", :released-at (time/local-date-time "2020-11-16T21:40:51.579") }))
